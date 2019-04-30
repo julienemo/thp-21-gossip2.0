3efc0a1c7ced6d60a25540b0a07d6d1b874aa1a2
Julie K <juliette.nada@gmail.com>
2019-04-30 14:14:41 +0200

    index page displaying gossip cards from the last one, so that the newly created be visible easily

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  get 'user/detail'
-  get 'tags/each'
+  get '/index', to: 'static_pages#index'
   get '/contact', to: 'static_pages#contact'
-
   get '/team', to: 'static_pages#team'
 
-  get '/index', to: 'static_pages#index'
+  get '/gossip/:id', to: 'gossip#each'
+  
+  get '/tag/:id', to: 'tags#each'
+  get 'user/:id', to: 'user#detail'
 
   get '/welcome/:id', to: 'welcome#show'
-
-  get '/gossip/:id', to: 'gossip#each'
-
-  get '/tag/:id', to: 'tags#each'
-
-  get 'user/:id', to: 'user#detail'
 end
